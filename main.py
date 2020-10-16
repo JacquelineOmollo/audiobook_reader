2e83666084b9f6543b7e747a8c7dd22a0742a311
Jacquelineomollo <Jacquelineomollo@gmail.com>
2020-10-15 23:37:39 -0400

    added read rate and changed voice to female

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import pyttsx3
 import PyPDF2
+import sys
 
+if not sys.warnoptions:
+    import warnings
+    warnings.simplefilter("ignore")
+
+bot = pyttsx3.init()
 book = open("Data Structures and Algorithms Using Python.pdf", "rb")
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print(pages)
-bot = pyttsx3.init()
+
+'''Rate'''
+rate = bot.getProperty("rate")
+print(rate)
+bot.setProperty("rate", 125)
+
+'''Voice'''
+voices = bot.getProperty("voices")
+print(voices)
+bot.setProperty("voice", voices[1].id)
+
 for i in range(20, pages):
     page = pdfReader.getPage(20)
     text = page.extractText()
     bot.say(text)
     bot.runAndWait()
+
+
+
+
+
